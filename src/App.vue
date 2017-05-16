200a114e9f4453266eb37a03a84702d7773dd5c2
hxl <2240932126@qq.com>
2017-05-16 19:46:30 +0800

    first
    Signed-off-by: hxl <2240932126@qq.com>

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<router-view></router-view>
+		<Header></Header>
+	</div>
+</template>
+
+<script>
+	import Header from '@/components/header/Header'
+	components: {
+			Header
+		}
+		export default {
+			name: 'app'
+		}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>{{msg}}</div>
+</template>
+
+<script>
+</script>
+export default{ name:'header', data () {return { msg: 'Welcome to Your Vue.js App' }} }
+<style>
+
+</style>
